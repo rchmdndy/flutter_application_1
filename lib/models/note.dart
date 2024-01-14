9e9506218a73a1bc31caa48b2a8a168431028797
rachmadandy <rachmadandyms@gmail.com>
2024-01-14 19:54:16 +0700

    first commit

---
+++ lib/models/note.dart
@@ -0,0 +1,68 @@
+final String tableNotes = 'notes';
+
+class NoteFields {
+  static final List<String> values = [
+    /// Add all fields
+    id, isImportant, number, title, description, createdTime
+  ];
+
+  static final String id = '_id';
+  static final String isImportant = 'isImportant';
+  static final String number = 'number';
+  static final String title = 'title';
+  static final String description = 'description';
+  static final String createdTime = 'createdTime';
+}
+
+class Note {
+  final int? id;
+  final bool isImportant;
+  final int number;
+  final String title;
+  final String description;
+  final DateTime createdTime;
+
+  const Note({
+    this.id,
+    required this.isImportant,
+    required this.number,
+    required this.title,
+    required this.description,
+    required this.createdTime,
+  });
+
+  static Note fromJson(Map<String, Object?> json) => Note(
+        id: json[NoteFields.id] as int?,
+        number: json[NoteFields.number] as int,
+        title: json[NoteFields.title] as String,
+        description: json[NoteFields.description] as String,
+        createdTime: DateTime.parse(json[NoteFields.createdTime] as String),
+        isImportant: json[NoteFields.isImportant] == 1,
+      );
+
+  Map<String, Object?> toJson() => {
+        NoteFields.id: id,
+        NoteFields.title: title,
+        NoteFields.description: description,
+        NoteFields.number: number,
+        NoteFields.isImportant: isImportant ? 1 : 0,
+        NoteFields.createdTime: createdTime.toIso8601String(),
+      };
+
+  Note copy({
+    int? id,
+    bool? isImportant,
+    int? number,
+    String? title,
+    String? description,
+    DateTime? createdTime,
+  }) =>
+      Note(
+        id: id ?? this.id,
+        isImportant: isImportant ?? this.isImportant,
+        number: number ?? this.number,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        createdTime: createdTime ?? this.createdTime,
+      );
+}
